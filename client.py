95324618e8b5eeb25a63b84ab3697d0e1e575f1f
Aditya Pal <adityapal.nghss@gmail.com>
2019-04-15 16:11:18 +0530

    Update client.py

--- client.py
+++ client.py
@@ -12,4 +12,22 @@ l = f.read(1024)
 while (l):
     s.send(l)
     l = f.read(1024)
+
+s.send(b'done')
+
+print(s.recv(1024).decode("utf-8"))
+print(s.recv(1024).decode("utf-8"))
+print(s.recv(1024).decode("utf-8"))
+
+f = open('k.txt', 'wb') 
+l = 1
+while(l):
+    l = s.recv(1024)
+    while (l):
+        f.write(l)
+        l = s.recv(1024)
+    f.close()
+
 s.close()
+
+
